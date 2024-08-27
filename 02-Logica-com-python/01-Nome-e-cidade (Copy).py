d4315e485367b6bfd08c19b96f6acc6006a889d9
Rom <wilson.mg.oliveira@gmail.com>
2024-08-27 12:42:36 -0300

    adicionando exercicio-02

---
+++ 02-Logica-com-python/01-Nome-e-cidade (Copy).py
@@ -0,0 +1,1 @@
+print("Olá Mundo") # Isto irá imprimir "Olá mundo! na tela
---
+++ 02-Logica-com-python/01-Nome-e-cidade.py
@@ -0,0 +1,8 @@
+# Lê o nome do usuário
+nome = input("Qual é o seu nome? ")
+
+# Lê a cidade do usuário
+cidade = input("Qual é sua cidade? ")
+
+# Imprime a mensagem
+print("Olá, meu nome é " + nome + " e minha cidade natal é " + cidade + ".")
